d16fdcfe74f542aa7dc3c377f99045dcc5d357dc
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-03 01:42:56 +0900

    added requirements.txt, chatpgt_sample.py

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python
 
+WORKDIR /app
+
 RUN apt-get update
 RUN pip install --upgrade pip
 RUN apt install -y tree
 
-# WORKDIR /app
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+# CMD ["python", "main.py"] # こいつを使いこなせるようになりたい
---
+++ src/chatgpt_sample.py
@@ -0,0 +1,14 @@
+import openai
+
+# gets API Key from environment variable OPENAI_API_KEY
+client = openai.OpenAI()
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "user",
+            "content": "Say this is a test",
+        }
+    ],
+    model="gpt-3.5-turbo",
+)
