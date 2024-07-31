6572746ee5c1c81c4baa913d359ca9e847c3e405
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-31 16:58:46 +0900

    modified dbacan_sample.py and added scikit-learn into requirement.txt

--- src/dbscan_sample.py
+++ src/dbscan_sample.py
@@ -1,2 +1,9 @@
 from sklearn.cluster import DBSCAN
 import numpy as np
+
+X = np.array([[1, 2], [2, 2], [2, 3],
+              [8, 7], [8, 8], [25, 80]])
+
+clustering = DBSCAN(eps=3, min_sample=2).fit(X)
+print("clusering.labels_", clustering.labels_)
+print("clustering: ", clustering)
