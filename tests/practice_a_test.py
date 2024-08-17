ccdab3f6ab8951a55c8c87c5a66cd2684d31bf65
shuhei0916 <shuheiito.stc@gmail.com>
2024-08-18 06:19:31 +0900

    Modify some files

---
+++ tests/practice_a_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from practice_a import print_abc
+
+class TestPracticeA(unittest.TestCase):
+    def test1(self):
+        self.
+
+if __name__ == '__main__':
+    unittest.main()
